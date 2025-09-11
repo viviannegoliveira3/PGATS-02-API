7da39aad7ab2c3636606008e7ad380ec14df7c9b
Vivianne Oliveira <vivianne.oliveira@gmail.com>
2025-09-11 19:38:21 -0300

    Habilitando a pipeline no Gitbub Actions

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,6 +6,9 @@ const { expect } = require ('chai');
 
 // Aplicação 
 const app = require('../../rest-api-app/rest-api-app/src/app');
+
+//Moch 
+const transferService = require ('../../rest-api-app/rest-api-app/src/services/transferService')
 // Testes
 describe('Transfer Controller', () => {
     describe('POST /api/transfer', () => {
@@ -19,12 +22,14 @@ describe('Transfer Controller', () => {
                 .send({ username: 'Cristiano', password: '123', favorecido: false });
             // Busca os usuários para pegar os IDs
             const usersRes = await request(app).get('/api/users');
-            const sender = usersRes.body.find(u => u.username === 'sender');
-            const recipient = usersRes.body.find(u => u.username === 'recipient');
+            const sender = usersRes.body.find(u => u.username === 'Vivianne');
+            const recipient = usersRes.body.find(u => u.username === 'Cristiano');
+            expect(sender).to.not.be.undefined;
+            expect(recipient).to.not.be.undefined;
             // Realiza a transferência
             const resposta = await request(app)
                .post('/api/transfer')
-               .send({ senderId: 1, recipientId: 1, amount: 100 });
+               .send({ senderId: sender.id, recipientId: recipient.id, amount: 100 });
             expect(resposta.status).to.equal(200);
             expect(resposta.body).to.have.property('message', 'Transfer successful');
         });
